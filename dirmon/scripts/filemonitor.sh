debeda480069da1aa181abf27db4d1445f5a9efd
bdrekapil <kapil.paliwal1@wipro.com>
2016-06-24 06:02:21 -0700

    updating execution script

--- dirmon/scripts/filemonitor.sh
+++ dirmon/scripts/filemonitor.sh
@@ -2,12 +2,14 @@
 . $(dirname $0)/../env.properties
 BDRE_HOME=~/bdre
 BDRE_APPS_HOME=~/bdre_app
-if [ -z "$1" ] || [ -z "$2" ] ; then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] ; then
         echo Insufficient parameters !
         exit 1
 fi
-processId=$1
-userName=$2
+busDomainId=$1
+processTypeId=$2
+processId=$3
+userName=$4
 echo $0
 #creating flume command for
-nohup java -cp "$BDRE_HOME/lib/file-mon/*" com.wipro.ats.bdre.filemon.FileMonRunnableMain -p $processId -u userName &
+nohup java -cp "$BDRE_HOME/lib/file-mon/*" com.wipro.ats.bdre.filemon.FileMonRunnableMain -p $processId -u $userName &
